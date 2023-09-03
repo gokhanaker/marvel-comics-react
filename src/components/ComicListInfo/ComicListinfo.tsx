b451de455b5925b04b2bb8b672c5f28018e5c935
gokhanaker <gokhanaker@gmail.com>
2023-09-03 18:43:38 +0200

    fix: fixed broken ui because of too long text from api

--- src/components/ComicListInfo/ComicListinfo.tsx
+++ src/components/ComicListInfo/ComicListinfo.tsx
@@ -1,36 +1,41 @@
 import { Comic, ComicListInfoProps } from '../../types';
 
+const maxLength = 40;
+const truncateText = (text: string, limit = maxLength) => {
+  if (text.length <= limit) return text;
+  return text.substring(0, limit) + '...';
+};
+
 const ComicListInfo = ({ comicList }: ComicListInfoProps) => {
   return (
     <div className="row">
       {comicList.map((comic: Comic) => (
-        <div>
-          <div className="card col s3" key={comic.id}>
-            <div className="card-image">
-              <img
-                src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
-                alt={comic.title}
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  maxWidth: '300px',
-                  maxHeight: '300px',
-                }}
-              />
-            </div>
-            <div className="card-content">
-              <h6
-                style={{
-                  display: 'flex',
-                  justifyContent: 'center',
-                  flexWrap: 'nowrap',
-                  margin: '0',
-                  fontSize: '12px',
-                }}
-              >
-                {comic.title}
-              </h6>
-            </div>
+        <div className="card col s3" key={comic.id}>
+          <div className="card-image">
+            <img
+              src={comic.thumbnail.path + '.' + comic.thumbnail.extension}
+              alt={comic.title}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                maxWidth: '300px',
+                maxHeight: '300px',
+              }}
+            />
+          </div>
+          <div className="card-content">
+            <h6
+              className="text-center"
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                flexWrap: 'nowrap',
+                margin: '0',
+                fontSize: '12px',
+              }}
+            >
+              {truncateText(comic.title)}
+            </h6>
           </div>
         </div>
       ))}
