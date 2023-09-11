9e4c440d77ce3e4ce2dd8c019cf9d07db0282af3
gokhanaker <gokhanaker@gmail.com>
2023-09-12 00:47:13 +0200

    test: added some unit tests for GetCharacter component

--- src/components/GetCharacter/__tests__/GetCharacter.test.tsx
+++ src/components/GetCharacter/__tests__/GetCharacter.test.tsx
@@ -1,10 +1,28 @@
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import GetCharacter from '../GetCharacter';
 
 describe('GetCharacter', () => {
-  test('Renders the component', () => {
-    render(<GetCharacter />);
-    expect(true).toBeTruthy();
+  test('renders the component', () => {
+    const { getByPlaceholderText } = render(<GetCharacter />);
+    expect(getByPlaceholderText('Search Your Character')).toBeInTheDocument();
+  });
+
+  test('updateCharacterName method should call setState method with heroName', () => {
+    const getCharacter = new GetCharacter({});
+    const setStateMock = jest.fn();
+    getCharacter.setState = setStateMock;
+
+    getCharacter.updateCharacterName({ target: { value: 'wolverine' } });
+    expect(setStateMock).toHaveBeenCalledWith({ heroName: 'wolverine' });
+  });
+
+  test('handleKeyPress method should call getCharacterInfo method when enter key is pressed', () => {
+    const getCharacter = new GetCharacter({});
+    const getCharacterInfoMock = jest.fn();
+    getCharacter.getCharacterInfo = getCharacterInfoMock;
+
+    getCharacter.handleKeyPress({ key: 'Enter' });
+    expect(getCharacterInfoMock).toHaveBeenCalled();
   });
 });
